d897842cedfc24e53bc973ceb08f838305aba893
maikashibata <134902066+maikashibata@users.noreply.github.com>
2023-07-28 07:54:51 +0000

    [33] 4-25

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get '/homes/about', to: 'homes#about'
-  resources :books, only: [:new, :index, :show]
+  get "/homes/about" => "homes#about", as: "about"
+  resources :books, only: [:new, :create, :index, :show]
   resources :users, only: [:show, :edit]
 end
