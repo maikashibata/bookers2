3afddf213b697c08f1287975fb3d73764144d2be
maikashibata <134902066+maikashibata@users.noreply.github.com>
2023-07-29 19:35:13 +0000

    [ファイルの記述・CRUDの実装] 4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to books_path(@book.id)
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
@@ -28,14 +28,13 @@ class BooksController < ApplicationController
   
   def edit
     @book = Book.find(params[:id])
-    @user = current_user
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to books_path(@book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to edit_user_path(@user.id)
+      redirect_to user_path(@user.id)
     else
       render :edit
     end
